abab8d204fd4efc2a504c2a7ec387d8d9dc85e4d
Sorokin1989 <sorokindm1try@yandex.ru>
2026-03-18 15:50:31 +0300

    create BonusService class

--- src/main/java/com/example/diplomproject/service/BonusService.java
+++ src/main/java/com/example/diplomproject/service/BonusService.java
@@ -17,6 +17,18 @@ public class BonusService {
 
 
     /**
+     * Начисление бонусов за регистрацию
+     */
+
+    public void addBonusForRegistration(User user){
+        if (user != null && user.getId() != null){
+            user.setBonusPoints(user.getBonusPoints()+BONUS_FOR_REGISTRATION);
+            userRepository.save(user);
+        }
+    }
+
+
+    /**
      * Начисление бонусов за оставленный отзыв
      */
 
@@ -26,4 +38,40 @@ public class BonusService {
             userRepository.save(user);
         }
     }
+
+    /**
+     * Начисление бонусов за первый заказ
+     */
+    public void addBonusForFirstOrder (User user) {
+        if (user != null && user.getId() != null && user.getOrders().size() == 1) {
+            user.setBonusPoints(user.getBonusPoints() + BONUS_FOR_FIRST_ORDER);
+            userRepository.save(user);
+        }
+    }
+
+    /**
+     * Списание бонусных баллов
+     * @return true, если успешно списано, иначе false
+     */
+        public boolean spendBonusPoints(User user, int bonusPoint){
+            if(user.getBonusPoints()>=bonusPoint){
+                user.setBonusPoints(user.getBonusPoints()-bonusPoint);
+                userRepository.save(user);
+                return true;
+            }
+            return false;
+        }
+    /**
+     * Получение текущего количества бонусов
+     */
+
+    public int getCurrentBonusPoints(User user){
+        if (user!=null){
+            return user.getBonusPoints();
+        }
+        return 0;
+    }
+
+
+
 }

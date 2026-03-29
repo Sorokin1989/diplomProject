7cf41507a28b7a57a74b2056e21a970ce775dee5
Sorokin1989 <sorokindm1try@yandex.ru>
2026-03-29 16:01:44 +0300

    update OrderStatus

--- src/main/java/com/example/diplomproject/enums/OrderStatus.java
+++ src/main/java/com/example/diplomproject/enums/OrderStatus.java
@@ -1,5 +1,18 @@
 package com.example.diplomproject.enums;
 
 public enum OrderStatus {
-    PENDING,PAID,CANCELLED,COMPLETED;
+    PENDING("Ожидает оплаты"),
+    PAID("Оплачен"),
+    COMPLETED("Завершён"),
+    CANCELLED("Отменён");
+
+    private final String displayName;
+
+    OrderStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }

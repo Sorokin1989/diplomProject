ef35161654f42d84195a1e12b4159fce9539cdb8
Sorokin1989 <sorokindm1try@yandex.ru>
2026-04-17 17:17:36 +0300

    update main.css, message.js

--- src/main/resources/static/js/message.js
+++ src/main/resources/static/js/message.js
@@ -1,7 +1,8 @@
 
     document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
-        document.querySelectorAll('.alert:not(.alert-permanent)').forEach(function(alert) {
+        document.querySelectorAll('.alert:not(.alert-permanent,.alert-info)').
+        forEach(function(alert) {
             alert.style.transition = 'opacity 0.5s';
             alert.style.opacity = '0';
             setTimeout(function() {
